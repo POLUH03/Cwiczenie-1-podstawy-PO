6b54246b9cb9a15a661df7063595e86d409f1030
LOukasz <Student@ST01-213>
2024-06-29 08:18:24 +0200

    Dodaj pliki projektów.

---
+++ Ćwiczenie 1 - podstawy PO/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Rectangle
+{
+    private double a;
+    private double b;
+    private double surface_area;
+
+    public void read_data()
+    {
+        Console.Write("Podaj długość boku a: ");
+        a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Podaj długość boku b: ");
+        b = Convert.ToDouble(Console.ReadLine());
+    }
+
+    public void process_data()
+    {
+        surface_area = a * b;
+    }
+
+    public void show_results()
+    {
+        Console.WriteLine($"Bok a: {a:F2}");
+        Console.WriteLine($"Bok b: {b:F2}");
+        Console.WriteLine($"Pole prostokąta: {surface_area:F2}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Rectangle rectangle = new Rectangle();
+        rectangle.read_data();
+        rectangle.process_data();
+        rectangle.show_results();
+    }
+}
